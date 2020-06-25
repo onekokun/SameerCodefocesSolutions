cf84875c8921080d7049575fbedfbefdfff390cb
Sameer <52498120+onekokun@users.noreply.github.com>
2020-06-25 19:36:58 +0530

    Create 281A.java

---
+++ 281A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class solution
+{
+ public static void main(String[] args)
+ {
+	Scanner scan = new Scanner(System.in);
+	String word = scan.next();
+	
+	System.out.print(word.substring(0,1).toUpperCase()+word.substring(1));
+	
+ }
+
+}
