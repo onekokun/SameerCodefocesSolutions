298f63eb30615570d6030c2269ef14002770c973
Sameer <52498120+onekokun@users.noreply.github.com>
2020-06-25 19:29:53 +0530

    Create 4A.java

---
+++ 4A.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class solution
+{
+ public static void main(String[] args)
+ {
+    Scanner scan = new Scanner(System.in);
+
+int a;
+ a = scan.nextInt();
+ if(a%2==0 && a>2)
+ System.out.print("YES");
+ else
+	 System.out.print("NO");    
+ }
+ 
+}
